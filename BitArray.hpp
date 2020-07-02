714d8c9aa9a1bda71868763cbab278e4e995e6f4
Youssef Bagoulla <ybagoulla@spectric.com>
2020-07-02 21:10:25 +0100

    Fixing my endian issues and adding to CMake

--- BitArray.hpp
+++ BitArray.hpp
@@ -5,6 +5,7 @@
 #include <endian.h>
 #include <assert.h>
 #include <functional>
+#include <stdint.h>
 
 class BitArray;
 
@@ -12,9 +13,8 @@ class BitArray;
  * A class to act as a proxy to a bit in an array.
  * Note that the _pos variable is:
  * Byte: [0 0 0 0 0 0 0 0]
- * _pos   7 6 5 4 3 2 1 0 
+ * _pos   0 1 2 3 4 5 6 7 
  *
- * This can be a little confusing b/c BitArray[0] is _pos 7 of the first ProxyBit.
  */
 class ProxyBit {
 friend class BitArray;
@@ -40,7 +40,7 @@ private:
 
 class BitArray {
 public:
-    BitArray(size_t len) : _size(len), _data(_size >> 3 + 1 + 7) {} // Very lazy, extra 7 bytes of zeros.
+    BitArray(size_t len) : _size(len), _data(_size/8 + (1 + 7)) {} // Very lazy, extra 7 bytes of zeros.
     BitArray() : _size(0), _data(0) {}
     size_t size() { return _size; };
     
@@ -49,7 +49,7 @@ public:
      */
     bool operator [](size_t i) const    {
         assert(i < _size);
-        return _data[i >> 3] & (1 << (7 - i & 7));
+        return _data[i / 8] & (1 << (i & 7));
     }
 
     /**
@@ -57,36 +57,31 @@ public:
      */
     ProxyBit operator [](size_t i) {
         assert(i < _size);
-        return ProxyBit(_data[i >> 3], (7 - i & 7));
+        return ProxyBit(_data[i / 8], (i & 7));
     }
 
     /**
      * Performs a dot product on a range of two ProxyBits.
      * Since the two ProxyBits could be offset we need to 
      * align them and do the given operator followed by the sum.
-     * Note that we perform these operations on 64-bit ints
-     * which on intel platform are little endian, we therefore
-     * need to convert them to big endian when doing our adjustments.
-     *
-     * Basically it's annoying.
      */
     template<class Func> static uint64_t DotProd(const ProxyBit & pb_a, const ProxyBit & pb_b, size_t len, Func func) {
         uint64_t accum(0);
-        uint64_t first_seven_mask = 0xFFFFFFFFFFFFFF00;
+        uint64_t first_seven_mask = 0x00FFFFFFFFFFFFFF;
 
         uint8_t * a_ptr = &pb_a._byte;
         uint8_t * b_ptr = &pb_b._byte;
 
-        for (size_t i = 0; len != 0; len -= (7<<3), i+=7) {
-            uint64_t a_64t = htobe64(*(uint64_t *) &a_ptr[i]);
-            a_64t = (a_64t << (7 - pb_a._pos));
+        for (size_t i = 0; len != 0; len -= (7 * 8), i+=7) {
+            uint64_t a_64t = (*(uint64_t *) &a_ptr[i]);
+            a_64t = (a_64t >> pb_a._pos);
 
-            uint64_t b_64t = htobe64(*(uint64_t *) &b_ptr[i]);
-            b_64t = (b_64t << (7 - pb_b._pos));
+            uint64_t b_64t = (*(uint64_t *) &b_ptr[i]);
+            b_64t = (b_64t >> pb_b._pos);
 
-            if (len < 7<<3) {
-                a_64t &= (0xFFFFFFFFFFFFFFFF << (64 - len));
-                b_64t &= (0xFFFFFFFFFFFFFFFF << (64 - len));
+            if (len < 7*8) {
+                a_64t &= (0xFFFFFFFFFFFFFFFF >> (64 - len));
+                b_64t &= (0xFFFFFFFFFFFFFFFF >> (64 - len));
                 accum += __builtin_popcountll(first_seven_mask & func(a_64t, b_64t));
                 break;
             }
