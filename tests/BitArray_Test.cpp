aa93676f13e193f9c58745bd3f38f8f4fed24a56
Michael Recachinas <mike@recachinas.dev>
2020-07-04 13:39:27 -0400

    Run clang-format on BitArray_*.cpp

--- tests/BitArray_Test.cpp
+++ tests/BitArray_Test.cpp
@@ -1,42 +1,43 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "BitArray.hpp"
+#include "doctest.hpp"
+#include <chrono>
 #include <limits.h>
 #include <stdlib.h>
 #include <vector>
-#include <chrono>
-#include "BitArray.hpp"
-#include "doctest.hpp"
 
 TEST_CASE("Testing creation") {
-    BitArray testEmpty;
-    CHECK(testEmpty.size() == 0);
+  BitArray testEmpty;
+  CHECK(testEmpty.size() == 0);
 
-    BitArray testNotEmpty(27);
-    CHECK(testNotEmpty.size() == 27);
+  BitArray testNotEmpty(27);
+  CHECK(testNotEmpty.size() == 27);
 
-    for (size_t i = 0; i < testNotEmpty.size(); i++)
-        CHECK(testNotEmpty[i] == 0);
+  for (size_t i = 0; i < testNotEmpty.size(); i++)
+    CHECK(testNotEmpty[i] == 0);
 }
 
 TEST_CASE("Testing DotProd") {
-    size_t size(5290), start_a(17), start_b(3), num(2370);
-    std::vector<short> testVec1(size), testVec2(size);
-    BitArray testArray1(size), testArray2(size);
-    srand(8);
-    for (size_t i = 0; i < testVec1.size(); ++i) {
-        testVec1[i] = rand()%2;
-        testArray1[i] = testVec1[i];
-        CHECK(testVec1[i] == testArray1[i]);
-        testVec2[i] = rand()%2;
-        testArray2[i] = testVec2[i];
-        CHECK(testVec2[i] == testArray2[i]);
-    }
+  size_t size(5290), start_a(17), start_b(3), num(2370);
+  std::vector<short> testVec1(size), testVec2(size);
+  BitArray testArray1(size), testArray2(size);
+  srand(8);
+  for (size_t i = 0; i < testVec1.size(); ++i) {
+    testVec1[i] = rand() % 2;
+    testArray1[i] = testVec1[i];
+    CHECK(testVec1[i] == testArray1[i]);
+    testVec2[i] = rand() % 2;
+    testArray2[i] = testVec2[i];
+    CHECK(testVec2[i] == testArray2[i]);
+  }
 
-    uint64_t expected_value(0);
-    for (size_t i = 0; i < num; ++i) {
-        expected_value += (testVec1[i+start_a] & testVec2[i+start_b]);
-    }
+  uint64_t expected_value(0);
+  for (size_t i = 0; i < num; ++i) {
+    expected_value += (testVec1[i + start_a] & testVec2[i + start_b]);
+  }
 
-    uint64_t my_value = BitArray::DotProd(testArray1[start_a], testArray2[start_b], num);
+  uint64_t my_value =
+      BitArray::DotProd(testArray1[start_a], testArray2[start_b], num);
 
-    CHECK(my_value == expected_value);
+  CHECK(my_value == expected_value);
 }
