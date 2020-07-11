de277ac4c063211ff7bbab943fe988191317d0a8
Youssef Bagoulla <ybagoulla@spectric.com>
2020-07-11 15:11:15 +0100

    Breaking bit counting out into its own function.
    This allows for older compilers and clang to compile

--- BitArray.hpp
+++ BitArray.hpp
@@ -16,6 +16,15 @@
 
 class BitArray;
 
+
+/**
+* Helper functions to count bits for both hardware enabled and non-hardware enabled platforms.
+*/
+__attribute__((target("popcnt"))) 
+inline int64_t countBits (const uint64_t &a) {  return _mm_popcnt_u64(a); }
+__attribute__((target("default")))
+inline int64_t countBits (const uint64_t &a) { return __builtin_popcountll(a); }
+
 /**
  * A class to act as a proxy to a bit in an array.
  * Note that the _pos variable is:
@@ -44,12 +53,8 @@ private:
   size_t _pos;
 };
 
-class BitArray {
 
-  struct SSE2Reg {
-    uint64_t upper64;
-    uint64_t lower64;
-  };
+class BitArray {
 
 public:
   BitArray(size_t len) : _size(len), _data(_size / 8 + (1 + 7)) {} // Very lazy, extra 7 bytes of zeros.
@@ -94,19 +99,21 @@ public:
       if (len < 7 * 8) {
         a_64t = (a_64t << (64 - len));
         b_64t = (b_64t << (64 - len));
-        accum += __builtin_popcountll(func(a_64t, b_64t));
+        accum += countBits(func(a_64t, b_64t));
         break;
       }
 
-      accum += __builtin_popcountll(first_seven_mask & func(a_64t, b_64t));
+      accum += countBits(first_seven_mask & func(a_64t, b_64t));
     }
 
     return accum;
   }
 
-  __attribute__((target("sse2", "popcnt"))) static uint64_t DotProd(const ProxyBit &pb_a, const ProxyBit &pb_b, size_t len) {
+  
+  __attribute__((target("sse2"))) 
+  static uint64_t DotProd(const ProxyBit &pb_a, const ProxyBit &pb_b, size_t len) {
     uint64_t accum(0);
-    SSE2Reg tmp;
+    uint64_t tmp[2];
 
     // Even with SIMD we still deal with 64-bit integers at a time (for now).
     // We load all we can (2 for SSE2, 4 for AVX etc.) of the first BitArray
@@ -122,8 +129,8 @@ public:
     for (; len > 112; len -= 112, i += 14) {
       // We deal with 8 bytes at a time but we drop the last byte, so we need
       // the SSE2 register to have first 8 bytes, Plus 1 byte of overlap.
-      tmp.lower64 = *(uint64_t *)&p_packedBits_A[i];
-      tmp.upper64 = *(uint64_t *)&p_packedBits_A[i + 7];
+      tmp[1] = *(uint64_t *)&p_packedBits_A[i];
+      tmp[0] = *(uint64_t *)&p_packedBits_A[i + 7];
       // Things look good here.
       __m128i a_bitVec = _mm_loadu_si128((__m128i const *)&tmp);
 
@@ -131,8 +138,8 @@ public:
       a_bitVec = _mm_srli_epi64(a_bitVec, pb_a._pos);
 
       // Load the next
-      tmp.lower64 = *(uint64_t *)&p_packedBits_B[i];
-      tmp.upper64 = *(uint64_t *)&p_packedBits_B[i + 7];
+      tmp[1] = *(uint64_t *)&p_packedBits_B[i];
+      tmp[0] = *(uint64_t *)&p_packedBits_B[i + 7];
       __m128i b_bitVec = _mm_loadu_si128((__m128i const *)&tmp);
 
       // Shift so it is also aligned
@@ -146,7 +153,7 @@ public:
       _mm_storeu_si128((__m128i *)&tmp, a_bitVec);
       // _mm_maskmoveu_si128 (a_bitVec, mask, (char*)&tmp); // TODO: I cant get
       // this to work, is it faster than shifts and store?
-      accum += _mm_popcnt_u64(tmp.lower64) + _mm_popcnt_u64(tmp.upper64);
+      accum += countBits(tmp[1]) + countBits(tmp[0]);
     }
 
     uint64_t first_seven_mask = 0x00FFFFFFFFFFFFFF;
@@ -166,16 +173,17 @@ public:
       if (len < 7 * 8) {
         a_64t = (a_64t << (64 - len));
         b_64t = (b_64t << (64 - len));
-        accum += _mm_popcnt_u64(a_64t & b_64t);
+        accum += countBits(a_64t & b_64t);
         break;
       }
 
-      accum += _mm_popcnt_u64(first_seven_mask & a_64t & b_64t);
+      accum += countBits(first_seven_mask & a_64t & b_64t);
     }
     return accum;
   }
 
-  __attribute__((target("default"))) static uint64_t DotProd(const ProxyBit &pb_a, const ProxyBit &pb_b, size_t len) {
+  __attribute__((target("default"))) 
+  static uint64_t DotProd(const ProxyBit &pb_a, const ProxyBit &pb_b, size_t len) {
     return DotProd(pb_a, pb_b, len, std::bit_and<uint64_t>());
   }
 
